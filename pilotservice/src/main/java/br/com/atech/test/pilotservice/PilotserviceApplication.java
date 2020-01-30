ec4c4edb17a2b6dd69be91372c720cdf476e504a
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-30 11:53:53 -0300

    Inclusao de pilotservice

---
+++ pilotservice/src/main/java/br/com/atech/test/pilotservice/PilotserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.atech.test.pilotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotserviceApplication.class, args);
+	}
+
+}
