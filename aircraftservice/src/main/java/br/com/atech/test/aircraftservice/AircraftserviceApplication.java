d2cdd29aa5ea3abb7d8f6da142558488c5820d0a
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-30 12:01:13 -0300

    Inclusao de pilotservice

---
+++ aircraftservice/src/main/java/br/com/atech/test/aircraftservice/AircraftserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.atech.test.aircraftservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftserviceApplication.class, args);
+	}
+
+}
