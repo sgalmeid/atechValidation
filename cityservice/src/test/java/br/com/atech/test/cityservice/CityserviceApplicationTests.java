49f605f20220b6db082157ac40dd9bd09f042d3b
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-30 12:05:49 -0300

    Inclusao de cityservice

---
+++ cityservice/src/test/java/br/com/atech/test/cityservice/CityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atech.test.cityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
