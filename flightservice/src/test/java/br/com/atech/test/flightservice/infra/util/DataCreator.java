9e383f96d17362dc16c6c471249bd9c80a25656d
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-31 06:28:53 -0300

    inclusao de classe utilitaria para testes

--- flightservice/src/test/java/br/com/atech/test/flightservice/infra/util/DataCreator.java
+++ flightservice/src/test/java/br/com/atech/test/flightservice/infra/util/DataCreator.java
@@ -4,9 +4,6 @@ import br.com.atech.test.flightservice.domain.Aircraft;
 import br.com.atech.test.flightservice.domain.City;
 import br.com.atech.test.flightservice.domain.Flight;
 import br.com.atech.test.flightservice.domain.Pilot;
-import br.com.atech.test.flightservice.infra.dto.AircraftDto;
-import br.com.atech.test.flightservice.infra.dto.CityDto;
-import br.com.atech.test.flightservice.infra.dto.PilotDto;
 import br.com.atech.test.flightservice.infra.dto.form.FlightFormDto;
 
 import java.time.LocalDateTime;
@@ -15,12 +12,12 @@ public class DataCreator {
 
     public static FlightFormDto getFlightFormDto(){
         return FlightFormDto.builder()
-                .aircraft(new AircraftDto(1L, "Emb 190"))
-                .arrivalCity(new CityDto(1L, "Sao Paulo"))
+                .aircraft(1L)
+                .arrivalCity(1L)
                 .arrivalTime(LocalDateTime.now().plusHours(13))
-                .departureCity(new CityDto(2L, "Londres"))
+                .departureCity(2L)
                 .departureTime(LocalDateTime.now().plusHours(2))
-                .pilot(new PilotDto(1L, "Carlos"))
+                .pilot(1L)
                 .build();
     }
 
