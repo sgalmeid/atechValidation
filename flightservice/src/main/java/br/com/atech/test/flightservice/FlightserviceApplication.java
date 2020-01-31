a0fba07c4bf3bd2ecae6aaec2e75d83575223b90
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-31 06:41:20 -0300

    inclusao de documentacao swagger e client side load balance

--- flightservice/src/main/java/br/com/atech/test/flightservice/FlightserviceApplication.java
+++ flightservice/src/main/java/br/com/atech/test/flightservice/FlightserviceApplication.java
@@ -3,9 +3,13 @@ package br.com.atech.test.flightservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
+@EnableFeignClients
 public class FlightserviceApplication {
 
 	public static void main(String[] args) {
