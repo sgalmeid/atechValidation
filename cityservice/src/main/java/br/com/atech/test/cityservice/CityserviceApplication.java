f9049126c31cfc71fea62ab8e1ff39f23c36278c
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-31 13:05:50 -0300

    Condifurando o service discovery e a documentacao do swagger

--- cityservice/src/main/java/br/com/atech/test/cityservice/CityserviceApplication.java
+++ cityservice/src/main/java/br/com/atech/test/cityservice/CityserviceApplication.java
@@ -2,8 +2,12 @@ package br.com.atech.test.cityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class CityserviceApplication {
 
 	public static void main(String[] args) {
