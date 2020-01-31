9a8405cf9468b0b4ada13c03a1fca5eb64018aba
Samuel Gobbo Almeida <samuel.almeida@gsw.com.br>
2020-01-31 06:23:25 -0300

    Inclusao de documentação usando swagger

--- pilotservice/src/main/java/br/com/atech/test/pilotservice/PilotserviceApplication.java
+++ pilotservice/src/main/java/br/com/atech/test/pilotservice/PilotserviceApplication.java
@@ -2,8 +2,12 @@ package br.com.atech.test.pilotservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class PilotserviceApplication {
 
 	public static void main(String[] args) {
